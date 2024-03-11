53ba25144debfaad624a8cedd2d3f7f496502915
s28470 <s28470@pjwstk.edu.pl>
2024-03-11 11:23:35 +0100

    created method that will find average in int tab

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,18 @@
         Console.WriteLine("Bye world!");
         Console.WriteLine("Bye world!");
     }
-    
+
+
+    public static double FindAverage(int[] tab)
+    {
+        double average = 0;
+        foreach (var i in tab)
+        {
+            average += i;
+        }
+
+        return average / tab.Length;
+    }
     
     
     
